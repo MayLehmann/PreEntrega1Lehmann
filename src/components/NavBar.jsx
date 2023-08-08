3427bf2633c876d2b3f4f0cd9125cfeaddcf33f0
Mayra Lehmann <mayra.lehmann@gmail.com>
2023-08-08 20:50:35 -0300

    agregé navbar de bootstrap

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">OverVan</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#features">Productos</Nav.Link>
+            <Nav.Link href="#pricing">Experiencias</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </> 
+  )
+ }
