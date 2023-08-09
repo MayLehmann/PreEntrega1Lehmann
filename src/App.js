3588ec2dc50db3591c0bba90d55c2aca33b511a5
Mayra Lehmann <mayra.lehmann@gmail.com>
2023-08-09 20:51:04 -0300

    linkeé bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { ItemListContainer } from './components/ItemListContainer';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import carrito from './multimedia/carrito compras.png';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-
-function App() {
+function ColorSchemesExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">OverVan</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#features">Productos</Nav.Link>
+            <Nav.Link href="#pricing">Experiencias</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <img src= {carrito} alt="carritoCompras"/> 0  
+    <ItemListContainer greeting = "Bienvenidx!"/> </>
+  )
+ }
 
-export default App;
+ export default ColorSchemesExample;
+//export default App
