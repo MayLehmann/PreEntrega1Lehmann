7c8e8112b35fcbfbff464f48a8ed04522d3bbfaa
Mayra Lehmann <mayra.lehmann@gmail.com>
2023-08-09 21:58:54 -0300

    agregué estilos

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,6 +2,6 @@ import carrito from '../multimedia/carrito compras.png'
 
 export const CartWidget = () => ( 
 <> 
-<img src= {carrito} alt="carrito de compras"/> 0 </> 
+<img src= {carrito} alt="carrito de compras"/> <span> 0 </span>  </> 
 
 );
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,3 +1,7 @@
-export const ItemListContainer = (props) => { 
-    return <div> {props.greeting}</div>
-};
+import Container from 'react-bootstrap/Container';
+
+export const ItemListContainer = (props) => (
+  <Container className="mt-4">
+    <h1>{props.greeting}</h1>
+  </Container>
+);
